eaeb11e2af2689304373167d0600964b7569a8bc
Jordy <jordy.pereira@hotmail.com>
2021-10-25 19:51:10 +0200

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  title: 'Wiki Jordy Pereira',
+  description: 'Just a summary of important links in life',
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    repo: 'jordypereira',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Homepage', link: 'https://jordypereira.be/' },
+    ],
+    sidebar: [
+      // {
+      //   title: 'Sweden - Government',
+      //   collapsable: true,
+      //   children: [
+      //     '/sweden/government/banks',
+      //   ],
+      // },
+      // {
+      //   title: 'Sweden - Healthcare',
+      //   collapsable: true,
+      //   children: [
+      //     '/sweden/healthcare/specialists',
+      //   ],
+      // },
+      // {
+      //   title: 'Sweden - Housing',
+      //   collapsable: true,
+      //   children: [
+      //     '/sweden/housing',
+      //   ],
+      // },
+    ],
+    sidebarDepth: 1,
+  },
+}
